b3d7afd6aa417195a01cca5d06c6e01febc12862
ever user <everuser@MacBook-Air-ever.local>
2020-12-11 11:49:25 +0200

    adaptive interruption

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+import layout from "./global/layout";
 import "select2/dist/js/select2.full";
 import Sliders from "./modules/Sliders";
 import Popup from "./modules/Popup";
@@ -5,6 +6,15 @@ import Popup from "./modules/Popup";
 $(function () {
   Sliders.init();
   Popup.init();
+  layout.layoutHandler({
+    afterResize: (layout) => {
+      $(".js-select--product").select2({
+        minimumResultsForSearch: Infinity,
+        placeholder: "Продукт",
+      });
+    },
+  });
+
   $(".js-select").select2({
     minimumResultsForSearch: Infinity,
   });
