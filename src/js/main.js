b39e533000fada65d2292e3168db5a7f6775c273
ever user <everuser@MacBook-Air-ever.local>
2020-12-11 11:09:51 +0200

    adaptive footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 import "select2/dist/js/select2.full";
 import Sliders from "./modules/Sliders";
+import Popup from "./modules/Popup";
 
 $(function () {
   Sliders.init();
+  Popup.init();
   $(".js-select").select2({
     minimumResultsForSearch: Infinity,
   });
---
+++ src/js/modules/Popup.js
@@ -0,0 +1,33 @@
+import noScroll from "../global/noScroll";
+const Popup = (function () {
+  "use strict";
+  const popUpBlock = $(".js-popup");
+  const linkShowPopUp = $(".js-show-popup");
+  const overlay = $(".js-overlay");
+  return {
+    initPopUp: function () {
+      linkShowPopUp.click(function (e) {
+        e.preventDefault();
+        const _this = $(this);
+        const target = $(_this.data("target"));
+        target.toggleClass("active");
+        overlay.addClass("active");
+        noScroll.on();
+      });
+    },
+    closePopup: function () {
+      $(".js-close, .js-overlay").click(function (e) {
+        e.preventDefault();
+        popUpBlock.removeClass("active");
+        overlay.removeClass("active");
+        noScroll.off();
+      });
+    },
+    init: function () {
+      Popup.initPopUp();
+      Popup.closePopup();
+    },
+  };
+})();
+
+export default Popup;
