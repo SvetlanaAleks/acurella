14fae0a94e78a90af43c16f42a46cf257abaca9a
ever user <everuser@MacBook-Air-ever.local>
2020-12-18 14:27:42 +0200

    add fixed menu

--- src/js/modules/Menu.js
+++ src/js/modules/Menu.js
@@ -1,10 +1,15 @@
 import noScroll from "../global/noScroll";
-
+import { DOC } from "../global/constants";
 const Menu = (function () {
   "use strict";
   const burgerMenu = $(".js-burger");
   const linkToTarget = $(".js-scroll");
   const overlay = $(".js-overlay");
+
+  const fixedMenu = $(".js-fixed-menu");
+  const menuHeight = fixedMenu.height();
+  const scrollHeight = menuHeight;
+
   function scroll(target) {
     const top = target.offset().top;
     $("html, body").animate(
@@ -15,6 +20,27 @@ const Menu = (function () {
     );
   }
   return {
+    showFixedMenu: function () {
+      DOC.scroll(function () {
+        const scrollTop = DOC.scrollTop();
+        let percent = 0;
+        let shadow = "none";
+        let bg = "none";
+        if (scrollTop < scrollHeight) {
+          percent = (scrollTop / scrollHeight).toFixed(2);
+          fixedMenu.removeClass("menu--fixed");
+        } else {
+          percent = 1;
+          shadow = "0px 0px 20px rgba(52, 49, 89, 0.1)";
+          bg = "white";
+          fixedMenu.addClass("menu--fixed");
+        }
+        fixedMenu.css({
+          boxShadow: shadow,
+          background: bg,
+        });
+      });
+    },
     showMobileMenu: function () {
       burgerMenu.click(function (e) {
         e.preventDefault();
@@ -49,6 +75,7 @@ const Menu = (function () {
     init: function () {
       Menu.showMobileMenu();
       Menu.scrollToTarget();
+      Menu.showFixedMenu();
     },
   };
 })();
